6e50478647e8e3d1ee7edd8e927a09cd454bbedf
Adam Turtle <themightybun@gmail.com>
2025-05-14 12:30:28 +0100

    Update APPBackgroundMode.m

--- src/ios/APPBackgroundMode.m
+++ src/ios/APPBackgroundMode.m
@@ -173,7 +173,8 @@ NSString* const kAPPBackgroundEventDeactivate = @"deactivate";
     // Play music even in background and dont stop playing music
     // even another app starts playing sound
     [session setCategory:AVAudioSessionCategoryPlayback
-                   error:NULL];
+                  withOptions:AVAudioSessionCategoryOptionMixWithOthers
+                  error:NULL];
 
     // Active the audio session
     [session setActive:YES error:NULL];
